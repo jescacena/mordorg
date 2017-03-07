fe5b68069f474391d8cda00b1b43704e6703c647
jescacena <fjescacena@paradigmatecnologico.com>
2017-03-07 13:07:13 +0100

    GPL license in landing and app

--- cercemap_app/components/Footer.jsx
+++ cercemap_app/components/Footer.jsx
@@ -5,8 +5,12 @@ export class Footer extends React.Component {
     return (
       <div className="ccm-footer">
         <span className="copyright" data-reactid=".0.3.4">
-          <span data-reactid=".0.3.4.0">Copyright © </span>
-          <a href="http://jesidea.com/" target="_blank" data-reactid=".0.3.4.1">JesIdea</a>
+
+        <a href="https://github.com/jescacena/mordorg/blob/master/LICENSE.txt" target="_blank">
+          <img src="img/gplv3-127x51.png" alt="Gnu Public License"/>
+          &nbsp;Copyright 2017 © Javier Escacena
+        </a>
+
         </span>
       </div>
     );
